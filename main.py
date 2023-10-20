588c5d80fffb2ac9a67ca9618dc13f6a134aa79b
cendecole <sahasa2014@rambler.ru>
2023-10-20 11:57:04 +0300

    sad6

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
 """""
+import random
+n=3
+a=list()
+for i in range(n):
+    b=list()
+    for i in  range(n):
+        b.append(int(round((random.random()*100),0)))
+    a.append(b)
+for i in range(n):
+    for j in range(n):
+        print(a[i][j], end =' ')
+    print()
+print(max(a[0][2],a[1][2],a[2][2]))
+print(max(a[1][0],a[1][1],a[1][2]))
+
+import random
 from random import randint
-l = [randint(10, 80) for x in range(10)]
-print(l)
-def pr(a):
-    l=a[0],a[-1]=a[-1],a[0]
-    return a
-print(pr(l))
-
-
-import math
-l = [6,12,5,15]
-a=l[0]*l[3]
-b=l[1]*l[2]
-if b==0:
-    print('fail')
-c=math.gcd(a, b)
-if b==1:
-    print(int(a/c))
-else:
-    print(int(a/c),'/',int(b/c))
+n=(int(round((random.random()*10),0)))
+m=(int(round((random.random()*10),0)))
+a=list()
+for i in range(n):
+    b=list()
+    for i in  range(m):
+        b.append(randint(-100,100))
+    a.append(b)
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end =' ')
+    print()
+print('-'*10)
+for i in range(n):
+    for j in range(m):
+        if a[i][j]>0:
+            a[i][j]=1
+        else:
+            a[i][j]=0
+for row in a:
+  for element in row:
+    print(element, end = ' ')
+  print()
 """""
-x=int(input('x='))
-y=int(input('y='))
-r=int(input('r='))
-if r^2!=x^2+y^2:
-    print('fail')
-
-
+import random
+from random import randint
+n=(int(round((random.random()*10),0)))
+a=list()
+for i in range(n):
+    b=list()
+    for i in  range(n):
+        b.append(randint(100))
+    a.append(b)
+for i in range(n):
+    for j in range(n):
+        print(a[i][j], end =' ')
+    print()
+for t in range(n):
+    if(sum[n-n][])
 
 
 
